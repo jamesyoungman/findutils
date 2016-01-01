48bf0d0c0dae8285ddfa657d274cc5dadda5752b
James Youngman <jay@gnu.org>
2016-01-01 12:37:20 +0000

    Add a shell script to generate ChangeLog; strip cherrypick comments.
    * build-aux/gen-changelog.sh: Shell script for generating the
    ChangeLog.  The effect is much the same as the previous version of
    the gen-ChangeLog rule, but it is easier to run this script to
    discover what the ChangeLog resulting from a recent commit is.
    The only change is that we now also use --strip-cherry-pick.
    * build-aux/Makefile.am (EXTRA_DIST): Distribute gen-changelog.sh.
    * Makefile.am (gen-ChangeLog): Simplify the gen-ChangeLog rule by
    calling gen-changelog.sh.

--- build-aux/gen-changelog.sh
+++ build-aux/gen-changelog.sh
@@ -1,8 +1,12 @@
-# Copyright (C) 2007,2015 Free Software Foundation, Inc.
+#! /bin/sh
+# Generate the ChangeLog for findutils.
+
+# Copyright (C) 2015 Free Software Foundation, Inc.
+# Written by James Youngman.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -13,4 +17,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-EXTRA_DIST = check-testfiles.sh man-lint.sh gen-changelog.sh
+set -u
+top_srcdir="$1"
+
+"${top_srcdir}/build-aux/gitlog-to-changelog" \
+    --srcdir="${top_srcdir}" \
+    --amend="${top_srcdir}/build-aux/git-log-fix" \
+    --since='2014-01-01' \
+    --strip-cherry-pick \
+  && cat "${top_srcdir}/ChangeLog-2013"
