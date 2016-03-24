c97c9c4f171f89e0ecdd47cebfc3564ba1fa9844
James Youngman <jay@gnu.org>
2016-03-24 06:53:06 +0000

    Adopt the use of the gnulib module "gendocs".
    * doc/.gitignore: Ignore gendocs_template and gendocs_template_min,
    since these are copied from gnulib/doc/.
    * cfg.mk (manual_title): Set $manual_title, since this is needed
    for maint.mk's nvocation of gendocs.sh.  Also set
    $gendocs_options_ so that the Texinfo tools can find
    dblocation.texi.
    * build-aux/.gitignore: Ignore gendocs.sh since it is copied into
    this directory by gnulib-tool (the file is part of the gendocs
    module).
    * bootstrap.conf (maintainer_modules): Add gendocs, so that we can
    automate the generation of HTML and other documentation for
    http://www.gnu.org/software/findutils.

--- cfg.mk
+++ cfg.mk
@@ -14,6 +14,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+manual_title = Finding Files
+# We need to pass the -I option to gendocs so that the texinfo tools
+# can find dblocation.texi, which is a generated file.
+gendocs_options_ = -s find.texi -I $(abs_builddir)/doc
+
 local-checks-to-skip :=
 
 # Errors I think are too picky anyway.
