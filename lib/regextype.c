4625495c480d846e1ca3a322509f1bed95a4d680
Bernhard Voelker <mail@bernhard-voelker.de>
2017-10-23 00:57:35 +0200

    maint: align copyright header line
    * lib/extendbuf.c: s/Copyright/Copyright (C)/
    * lib/printquoted.h: Likewise.
    * lib/regextype.c: Likewise.
    * lib/regextype.h: Likewise.

--- lib/regextype.c
+++ lib/regextype.c
@@ -2,7 +2,7 @@
 /* regextype.c -- Decode the name of a regular expression syntax into am
                   option name.
 
-   Copyright 2005-2017 Free Software Foundation, Inc.
+   Copyright (C) 2005-2017 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
--- lib/regextype.h
+++ lib/regextype.h
@@ -1,6 +1,6 @@
 /* regextype.h -- Decode the name of a regular expression syntax.
 
-   Copyright 2005-2017 Free Software Foundation, Inc.
+   Copyright (C) 2005-2017 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
