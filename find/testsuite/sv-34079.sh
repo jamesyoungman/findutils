15f9a4903c9dc915cd2ec6bf98cb8423c54bc28e
Bernhard Voelker <mail@bernhard-voelker.de>
2018-02-05 01:33:23 +0100

    tests: skip sv-34079.sh properly if setting up the test fails
    The test exited as a test failure (exit 1) also when set-up failed,
    e.g. when running the test with TMPDIR on a file system with too
    few free inodes.  Instead, the test should exit with 77 to skip the
    test.  In other cases, exit properly with 99 to signal a test
    framework failure.
    
    * find/testsuite/sv-34079.sh (very_expensive_): Move the check for
    RUN_VERY_EXPENSIVE_TESTS to the beginning, thus decreasing the
    indentation level of the test code.  This is also a preparation
    step for moving this check to a central place.
    (skip_,framework_failure_): Define utility functions to skip the test
    or terminate it as a test framework failure.
    (get_ifree_): Define function to determine the number of free inodes.
    (make_test_data): Use it, and skip early if we know that there are too
    few free inodes on the current file system.
    Use "seq -f" to build the file names, and directly feed them via xargs
    to touch.  This avoids slower shell code.
    Skip the test if touch fails; previously, the test exited as FAIL.
    Move the creation of test files up, i.e., outside the if-condition,
    and use trap to remove the temporary directory at exit.

--- find/testsuite/sv-34079.sh
+++ find/testsuite/sv-34079.sh
@@ -17,67 +17,125 @@
 
 # This test verifies that find does not have excessive memory consumption
 # even for large directories.   It's not executed by default; it will only
-# run if the environment variable RUN_VERY_EXPENSIVE_TESTS is set.
+# run if the environment variable RUN_VERY_EXPENSIVE_TESTS=yes is set.
 
 testname="$(basename $0)"
 
 . "${srcdir}"/binary_locations.sh
 
-# Require seq(1) for this test - which may not be available
-# on some systems, e.g on some *BSDs.
-seq 2 >/dev/null 2>&1 \
-  || { echo "$testname: required utility 'seq' missing" >&2; exit 77; }
-
-make_test_data() {
-    d="$1"
-    (
-	cd "$1" && echo "Creating test data in $(pwd -P)" >&2 || exit 1
-	max=400
-	for i in $(seq 0 400)
-	do
-	    printf "\r%03d/%03d" $i $max >&2
-	    for j in $(seq 0 10000)
-	    do
-		printf "%03d_%04d " $i $j
-	    done
-	done | xargs sh -c 'touch "$@" || exit 255' fnord || exit 1
-	printf "\rTest files created.\n" >&2
-    )
+skip_() { # TODO factor out to a central place.
+  echo "$testname: $@" >&2
+  exit 77;
 }
 
+framework_failure_() { # TODO factor out to a central place.
+  echo "$testname: error during test set-up" >&2
+  exit 99;
+}
 
-if [ -n "${RUN_VERY_EXPENSIVE_TESTS}" ]; then
-    if outdir=$(mktemp -d); then
-	# Create some test files.
-	bad=""
-	printf "Generating test data in %s (this may take some time...):\n" \
-	    "${outdir}" >&2
-	if make_test_data "${outdir}"; then
-	    # We don't check oldfind, as it uses savedir, meaning that
-	    # it stores all the directory entries.  Hence the excessive
-	    # memory consumption bug applies to oldfind even though it is
-	    # not using fts.
-	    for exe in "${ftsfind}" "${oldfind}"; do
-	        echo "Checking memory consumption of ${exe}..." >&2
-                if ( ulimit -v 50000 && ${exe} "${outdir}" >/dev/null; ); then
-                        echo "Memory consumption of ${exe} is reasonable" >&2
-                else
-                        bad="${bad}${bad:+\n}Memory consumption of ${exe} is too high"
-                fi
-	    done
-	else
-	    bad="failed to set up the test in ${outdir}"
-	fi
-	rm -rf "${outdir}" || exit 1
-	if [ -n "${bad}" ]; then
-	    echo "${bad}" >&2
-	    exit 1
-	fi
-    else
-	echo "FAIL: could not create a test output file." >&2
-	exit 1
-    fi
-else
-    echo "${testname} was not run because" '${RUN_VERY_EXPENSIVE_TESTS}' \
-	"is unset."
-fi
+very_expensive_() { # TODO factor out to a central place.
+  if test "$RUN_VERY_EXPENSIVE_TESTS" != yes; then
+    echo 'very expensive: disabled by default
+This test is very expensive, so it is disabled by default.
+To run it anyway, rerun make check with the RUN_VERY_EXPENSIVE_TESTS
+environment variable set to yes.  E.g.,
+
+  env RUN_VERY_EXPENSIVE_TESTS=yes make check
+' >&2
+  exit 77
+  fi
+}
+very_expensive_
+
+# Require seq(1) for this test - which may not be available
+# on some systems, e.g on some *BSDs.
+seq -f "_%04g" 0 2 >/dev/null 2>&1 \
+  || skip_ "required utility 'seq' missing"
+
+# Get the number of free inodes on the file system of the given file/directory.
+get_ifree_() {
+  d="$1"
+  d="$1"
+  # Try GNU coreutils' stat.
+  stat --format='%d' -f -- "$d" 2>/dev/null \
+    && return 0
+
+  # Fall back to parsing 'df -i' output.
+  df -i -- "$d" \
+  | awk '
+      NR == 1 {  # Find ifree column.
+        ifree = -1;
+        for (i=1; i<=NF; i++) {
+          n=tolower($i);
+          if(n=="ifree" || n=="iavail") {
+            ifree=i;
+          }
+        };
+        if (ifree<=0) {
+          print "failed to determine IFREE column in header: ", $0 | "cat 1>&2";
+          exit 1;
+        }
+        next;
+      }
+    { print $ifree }
+  ' \
+  | grep .
+}
+
+make_test_data() {
+  d="$1"
+  (
+    cd "$1" || framework_failure_
+    echo "Creating test data in '$(pwd -P)' (this may take some time...)" >&2
+
+    maxi=400; maxj=10000
+
+    # Skip early if we know that there are too few free inodes.
+    # Require some slack.
+    free_inodes=$(get_ifree_ '.') \
+      && test 0 -lt $free_inodes \
+      && min_free_inodes=$(expr 12 \* $maxi \* $maxj / 10) \
+      && { test $min_free_inodes -lt $free_inodes \
+             || skip_ "too few free inodes on '.': $free_inodes;" \
+                      "this test requires at least $min_free_inodes"; }
+
+    for i in $(seq -f "%03g" 0 $maxi)
+    do
+      printf "\r${i}/${maxi}" >&2
+      seq -f "${i}_%04g" 0 $maxj | xargs touch || skip_ "touch failed"
+    done
+    printf "\rTest files created.\n" >&2
+  )
+}
+
+# Remove the temporary directory and exit with the incoming value of $?.
+remove_outdir_ ()
+{
+  __st=$?
+  rm -rf "${outdir}" || { test $__st = 0 && __st=1; }
+  exit $__st
+}
+
+outdir=$(mktemp -d) || framework_failure_
+trap remove_outdir_ 0
+
+# Create some test files.
+make_test_data "${outdir}" \
+  || skip_ "failed to set up the test in '${outdir}'"
+
+fail=0
+# We don't check oldfind, as it uses savedir, meaning that
+# it stores all the directory entries.  Hence the excessive
+# memory consumption bug applies to oldfind even though it is
+# not using fts.
+for exe in "${ftsfind}" "${oldfind}"; do
+  echo "Checking memory consumption of ${exe}..." >&2
+  if ( ulimit -v 50000 && ${exe} "${outdir}" >/dev/null; ); then
+    echo "Memory consumption of ${exe} is reasonable" >&2
+  else
+    echo "${exe}: memory consumption is too high"
+    fail=1
+  fi
+done
+
+exit $fail
