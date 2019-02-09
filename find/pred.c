jamesyoungman/findutils

commit 7a6e548690b116878070665bd9844275823bb730

find: handle more readdir(3) errors
Similar to the FTS readdir fix in v4.6.0-72-g155c9d1, handle the last
two unhandled readdir(3) errors.

* find/pred.c (pred_empty): Do the above.
* lib/fdleak.c (get_proc_max_fd): Likewise.  While at it, fix the
condition to only skip "." and ".."; previously, also other files
beginning with ".." would have been skipped - that was theoretically,
as we only expect the FD files in "/proc/self/fd".

find/pred.c
@@ -380,6 +380,9 @@ pred_empty (const char *pathname, struct stat *stat_buf, struct predicate *pred_
 	  state.exit_status = 1;
 	  return false;
 	}
+      /* errno is not touched in the loop body, so initializing it here
+       * once before the loop is enough to detect readdir(3) errors.  */
+      errno = 0;
       for (dp = readdir (d); dp; dp = readdir (d))
 	{
 	  if (dp->d_name[0] != '.'
@@ -389,6 +392,13 @@ pred_empty (const char *pathname, struct stat *stat_buf, struct predicate *pred_
 	      empty = false;
 	      break;
 	    }
+	}
+      if (errno)
+	{
+	  /* Handle errors from readdir(3). */
+	  error (0, errno, "%s", safely_quote_err_filename (0, pathname));
+	  state.exit_status = 1;
+	  return false;
 	}
       if (CLOSEDIR (d))
 	{

lib/fdleak.c
@@ -19,6 +19,7 @@
 
 /* system headers. */
 #include <assert.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <poll.h>
@@ -77,11 +78,23 @@ get_proc_max_fd (void)
       int good = 0;
       struct dirent *dent;
 
-      while ((dent=readdir (dir)) != NULL)
-	{
+      while (1)
+        {
+	  errno = 0;
+	  dent = readdir (dir);
+	  if (NULL == dent)
+	    {
+	      if (errno)
+		{
+		  error (0, errno, "%s", quotearg_n_style (0, locale_quoting_style, path));
+		  good = 0;
+		}
+	      break;
+	    }
+
 	  if (dent->d_name[0] != '.'
-	      || (dent->d_name[0] != 0
-		  && dent->d_name[1] != 0 && dent->d_name[1] != '.'))
+	      || (dent->d_name[1] != 0
+		  && (dent->d_name[1] != '.' || dent->d_name[2] != 0)))
 	    {
 	      const int fd = safe_atoi (dent->d_name, literal_quoting_style);
 	      if (fd > maxfd)

commit 235f21673cd8e3458795cdadca6e78a3423ab024

find: make pred_empty safer and avoid fd leaks
There is a small race condition between the previous stat call
and openat/fdopendir (which cannot be avoided): if the directory
got replaced by another file type, then openat would succeed but the
subsequent fdopendir would fail with ENOTDIR.  Detect this earlier
by passing the O_DIRECTORY flag.
Furthermore, the opened file descriptor was leaked in that case
(bug introduced in FINDUTILS_4_3_2-1-80-gb46b0d89 in 2007).
Later on, after a readdir error, also the directory stream was leaked
(bug introduced by myself in commit 7a6e548690b1).

* find/pred.c (pred_empty): Add more flags to the openat call,
especially O_DIRECTORY; inspired by gnulib's opendirat module.
Close the file descriptor when fdopendir failed.
Close the directory stream when readdir failed.

find/pred.c
@@ -363,9 +363,9 @@ pred_empty (const char *pathname, struct stat *stat_buf, struct predicate *pred_
       errno = 0;
       if ((fd = openat (state.cwd_dir_fd, state.rel_pathname, O_RDONLY
 #if defined O_LARGEFILE
-			|O_LARGEFILE
+			| O_LARGEFILE
 #endif
-		       )) < 0)
+			| O_CLOEXEC | O_DIRECTORY | O_NOCTTY | O_NONBLOCK)) < 0)
 	{
 	  error (0, errno, "%s", safely_quote_err_filename (0, pathname));
 	  state.exit_status = 1;
@@ -376,6 +376,7 @@ pred_empty (const char *pathname, struct stat *stat_buf, struct predicate *pred_
 	{
 	  error (0, errno, "%s", safely_quote_err_filename (0, pathname));
 	  state.exit_status = 1;
+	  close (fd);
 	  return false;
 	}
       /* errno is not touched in the loop body, so initializing it here
@@ -396,6 +397,7 @@ pred_empty (const char *pathname, struct stat *stat_buf, struct predicate *pred_
 	  /* Handle errors from readdir(3). */
 	  error (0, errno, "%s", safely_quote_err_filename (0, pathname));
 	  state.exit_status = 1;
+	  CLOSEDIR (d);
 	  return false;
 	}
       if (CLOSEDIR (d))