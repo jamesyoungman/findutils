db5ec614a70ebd847bf51288293a0cd0c87741ae
Bernhard Voelker <mail@bernhard-voelker.de>
2022-01-02 18:42:35 +0100

    maint: update copyright year number ranges
    Run 'make update-copyright'.
    
    * lib/regexprops.c (copying): Update the year number manually.
    The format of the copyright year number range in Texinfo files is
    "YEAR1--YEAR2" now, i.e., with 2x '-'.
    * tests/sample-test: Adjust to use the single most recent year.
    * All other files: Update copyright years via the above make run.

--- lib/qmark.c
+++ lib/qmark.c
@@ -1,6 +1,6 @@
 /* qmark.c -- quote 'dangerous' filenames
    Derived from coreutils' ls.c.
-   Copyright (C) 1985-2021 Free Software Foundation, Inc.
+   Copyright (C) 1985-2022 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
