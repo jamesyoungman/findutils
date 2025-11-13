99cfb6cf42679cf826dbfb5616fd91199617dc16
James Youngman <james@youngman.org>
2025-11-13 22:13:18 +0000

    [maint] Stricter manpage checks.
    From a suggestion by G. Branden Robinson to use `groff -t -z -ww
    -rCHECKSTYLE=2 -man`, by pointing to and example in
    https://lists.gnu.org/archive/html/bug-findutils/2025-11/msg00004.html.
    
    * configure.ac: discover the location of the groff binary.
    * build-aux/man-lint.sh: also run groff with -rCHECKSTYLE=2.   Use
      groff at the path discovered during configuration.
    * find/Makefile.am: Lint-check manpages during "make check",
      not "make dist". Set the environment variable GROFF to the
      location of the groff binary.
    * locate/Makefile.am: Likewise.
    * xargs/Makefile.am: Likewise.

--- build-aux/man-lint.sh
+++ build-aux/man-lint.sh
@@ -14,20 +14,60 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-rv=0
+case "${GROFF}" in
+    :)
+	echo "groff is not installed, so we cannot check manual pages.  Continuing without checking them." >&2
+	exit 0
+	;;
+    "")
+	echo "The GROFF environment is not set; this is normally set when invoking this command from the Makefile; assuming GNU groff is at 'groff'." >&2
+	GROFF=groff
+	;;
+    *)
+	;;
+esac
+
 srcdir="$1" ; shift
 
-for manpage
-do
-  what="lint check on manpage $manpage"
-  echo -n "$what: "
-  messages="$( groff -t -man ${srcdir}/${manpage} 2>&1 >/dev/null )"
-  if test -z "$messages" ; then
-      echo "passed"
-  else
-      echo "FAILED:" >&2
-      echo "$messages"     >&2
-      rv=1
-  fi
-done
-exit $rv
+fixed_width_context_message_without_newline() {
+    printf '%-45s (%15s): ' "$1" "$2"
+}
+
+
+check_manpages_format_without_error_messages() {
+    for manpage
+    do
+	fixed_width_context_message_without_newline \
+		 'check_manpages_format_without_error_messages' "${manpage}"
+	messages="$( ${GROFF} -t -man ${srcdir}/${manpage} 2>&1 >/dev/null )"
+	if test -z "$messages"
+	then
+	    printf 'OK\n'
+	else
+	    printf 'FAILED\n%s\n' "$messages" >&2
+	    return 1
+	fi
+    done
+    return 0
+}
+
+check_manpages_with_groff_checkstyle_2() {
+    for manpage
+    do
+	fixed_width_context_message_without_newline \
+		 'check_manpages_with_groff_checkstyle_2' "${manpage}"
+	messages="$( ${GROFF} -t -z -ww -rCHECKSTYLE=2 -man ${srcdir}/${manpage} 2>&1 )"
+	if test -z "$messages"
+	then
+	    printf 'OK\n'
+	else
+	    printf 'FAILED\n%s\n' "$messages" >&2
+	    return 1
+	fi
+    done
+    return 0
+}
+
+rv=0
+check_manpages_format_without_error_messages "$@" &&
+check_manpages_with_groff_checkstyle_2       "$@"
